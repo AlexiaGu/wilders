6764b7ec42a7a098e463a26352fb90b7c1147e8f
AlexiaGu <alexiaguidet@gmail.com>
2023-12-05 10:54:55 +0100

    essai node

---
+++ index.js
@@ -0,0 +1,13 @@
+const names = ["Axelle", "Cecile", "Damien", "Farah"];
+
+
+    console.log(chalk.blue("Axelle"));
+    console.log(chalk.green("Cecile"));
+    console.log(chalk.red("Damien"));
+    console.log(chalk.yellow("Farah"));
+
+
+
+
+import chalk from 'chalk';
+// console.log(chalk.blue (names))
